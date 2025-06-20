6c08bd90c528e5820cf2ed8a373000c07a1afca2
GitHub Action <action@github.com>
2025-06-20 20:40:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/oliwiertas/xd4min3torkits10/MyBundle.kt
+++ src/main/kotlin/com/github/oliwiertas/xd4min3torkits10/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.oliwiertas.xd4min3torkits10
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/oliwiertas/xd4min3torkits10/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/oliwiertas/xd4min3torkits10/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.oliwiertas.xd4min3torkits10.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
